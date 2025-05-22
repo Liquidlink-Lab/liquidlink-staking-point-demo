0e8b78e35382b8bcba6e4b6b65a303e006d83e15
aaron <ru19wu06@gmail.com>
2025-05-22 10:38:12 +0800

    update unstake

--- my-website/components/actions.tsx
+++ my-website/components/actions.tsx
@@ -27,20 +27,30 @@ import {
 } from 'lucide-react';
 import { useToast } from '@/components/ui/use-toast';
 import { Slider } from '@/components/ui/slider';
+import { IotaClient,getFullnodeUrl } from '@iota/iota-sdk/client';
 
 const PACKAGE_ID =
   '0xe87fadb56ac565aa46d60c1f5fa30b22eb10b6ea0763da70d9eb75adb75fd0b3';
 const ACTION = 'stake';
+const ACTION2 = 'unstake';
 const LOCK_PERIOD = 0;
 const TREASURY_CAP_OBJECT_ID =
   '0x91e82a7b9b2b5dfb0993eb01604d9172dc93c809850ca8b470fcd488feaea0b3';
 const VAULT_OBJECT_ID =
   '0x7e8e05366388d163257d7d7427293db6795284f5e961cb6244c7273bb28ee652';
 
+const CERT_TYPE = '0x2::coin::Coin<0xe87fadb56ac565aa46d60c1f5fa30b22eb10b6ea0763da70d9eb75adb75fd0b3::cert::CERT>'
+
+const client = new IotaClient({
+  url: getFullnodeUrl('testnet'),
+});
+
+
 export function Actions() {
   const [stakingAmount, setStakingAmount] = useState('');
-  const [lendingAmount, setLendingAmount] = useState('');
   const [withdrawAmount, setWithdrawAmount] = useState('');
+  const [vIotaCoins, setVIotaCoins] = useState(0);
+
   const [lockPeriod, setLockPeriod] = useState(30);
   const { toast } = useToast();
 
@@ -62,7 +72,9 @@ export function Actions() {
 
     const tx = new Transaction();
 
+
     const [coin] = tx.splitCoins(tx.gas, [stakingAmount]);
+
     tx.moveCall({
       target: `${PACKAGE_ID}::core::stake`,
       arguments: [
@@ -102,28 +114,9 @@ export function Actions() {
     );
   };
 
-  const handleLend = () => {
-    if (
-      !lendingAmount ||
-      isNaN(Number(lendingAmount)) ||
-      Number(lendingAmount) <= 0
-    ) {
-      toast({
-        title: 'Error',
-        description: 'Please enter a valid lending amount',
-        variant: 'destructive',
-      });
-      return;
-    }
 
-    toast({
-      title: 'Lending Successful',
-      description: `You have successfully lent ${lendingAmount} IOTA`,
-    });
-    setLendingAmount('');
-  };
 
-  const handleWithdraw = () => {
+  const handleWithdraw = async() => {
     if (
       !withdrawAmount ||
       isNaN(Number(withdrawAmount)) ||
@@ -131,18 +124,88 @@ export function Actions() {
     ) {
       toast({
         title: 'Error',
-        description: 'Please enter a valid withdrawal amount',
+        description: 'Please enter a valid staking amount',
         variant: 'destructive',
       });
       return;
     }
 
-    toast({
-      title: 'Withdrawal Successful',
-      description: `You have successfully withdrawn ${withdrawAmount} IOTA`,
+    const tx = new Transaction();
+    //const [coin] = tx.splitCoins(tx.gas, [stakingAmount]);
+    const tokenObject = await getCertObject()
+    const [coin] = tx.splitCoins(
+      tx.object(tokenObject),
+      [withdrawAmount]
+    );
+
+    tx.moveCall({
+      target: `${PACKAGE_ID}::core::unstake`,
+      arguments: [
+        tx.pure.string(ACTION2),
+        coin,
+        tx.pure.u64(LOCK_PERIOD),
+        tx.object(IOTA_CLOCK_OBJECT_ID),
+        tx.object(TREASURY_CAP_OBJECT_ID),
+        tx.object(VAULT_OBJECT_ID),
+      ],
     });
-    setWithdrawAmount('');
+
+    signAndExecuteTransaction(
+      {
+        transaction: tx,
+        chain: 'iota:testnet',
+      },
+      {
+        onSuccess: (result) => {
+          console.log('executed transaction', result);
+          toast({
+            title: 'Staking Successful',
+            description: `You have successfully staked ${withdrawAmount} IOTA`,
+          });
+          setStakingAmount('');
+        },
+
+        onError: (error) => {
+          console.error('error', error);
+          toast({
+            title: 'Staking Failed',
+            description: `You have failed to stake ${withdrawAmount} IOTA`,
+            variant: 'destructive',
+          });
+        },
+      },
+    );
   };
+
+  const getCertObject = async() => {
+    //user: string
+    const user2 = '0x44cbb06b6783eed0b5411ad76eb05ff2562b132daa519484141304f37d1c24c3';
+    const vIotaCoins = await client.getOwnedObjects({
+      owner: user2,
+      options: { showType: true, showContent: true },
+    });
+
+    let colloctBook = 0;
+    let objectId;
+    vIotaCoins.data.map((index) => {
+      if (index.data?.type === CERT_TYPE) {
+
+        const bal = parseInt((index as any).data?.content?.fields?.balance);
+        //console.log(typeof bal)
+        if (typeof bal === 'number' && bal > colloctBook) {
+          colloctBook = bal;
+
+          objectId = index
+        }
+
+      }
+
+    });
+    console.log(colloctBook)
+    console.log(objectId?.data)
+
+    return objectId?.data
+  }
 
   const handleFreeTokenClaim = () => {
     toast({
@@ -266,73 +329,6 @@ export function Actions() {
           </Card>
         </TabsContent>
 
-        <TabsContent value="lend">
-          <Card className="bg-gray-900 border-gray-800">
-            <CardHeader>
-              <CardTitle className="text-white flex items-center">
-                <ArrowUpFromLine className="mr-2 h-5 w-5 text-[#00e0c6]" />
-                Lend IOTA
-              </CardTitle>
-              <CardDescription className="text-gray-400">
-                Lend your IOTA tokens to earn interest
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <div className="space-y-4">
-                <div className="space-y-2">
-                  <div className="flex justify-between">
-                    <label className="text-sm font-medium leading-none text-gray-300 peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                      Lending Amount
-                    </label>
-                    <span className="text-xs text-gray-500">
-                      Available Balance: 2,500 IOTA
-                    </span>
-                  </div>
-                  <div className="flex space-x-2">
-                    <Input
-                      type="number"
-                      placeholder="Enter amount"
-                      value={lendingAmount}
-                      onChange={(e) => setLendingAmount(e.target.value)}
-                      className="bg-gray-800 border-gray-700 text-white"
-                    />
-                    <Button
-                      variant="outline"
-                      className="whitespace-nowrap border-gray-700 text-gray-300 hover:bg-gray-800 hover:text-white"
-                      onClick={() => setLendingAmount('2500')}
-                    >
-                      Max
-                    </Button>
-                  </div>
-                </div>
-
-                <div className="rounded-lg bg-gray-800 p-4 border border-gray-700">
-                  <div className="flex justify-between text-sm mb-2">
-                    <span className="text-gray-400">Lending Rate</span>
-                    <span className="font-medium text-white">8.2%</span>
-                  </div>
-                  <div className="flex justify-between text-sm">
-                    <span className="text-gray-400">Lending Term</span>
-                    <span className="font-medium text-white">30 days</span>
-                  </div>
-                  <div className="flex justify-between text-sm mt-2">
-                    <span className="text-gray-400">Collateralization</span>
-                    <span className="font-medium text-white">150%</span>
-                  </div>
-                </div>
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button
-                className="w-full bg-gradient-to-r from-[#00a0b0] to-[#00e0c6] hover:from-[#008a99] hover:to-[#00c6af] text-black font-medium"
-                onClick={handleLend}
-              >
-                <ArrowUpFromLine className="mr-2 h-4 w-4" />
-                Lend Now
-              </Button>
-            </CardFooter>
-          </Card>
-        </TabsContent>
 
         <TabsContent value="withdraw">
           <Card className="bg-gray-900 border-gray-800">
@@ -397,6 +393,13 @@ export function Actions() {
               >
                 <ArrowDownToLine className="mr-2 h-4 w-4" />
                 Withdraw Now
+              </Button>
+              <Button
+                className="w-full bg-gradient-to-r from-[#00a0b0] to-[#00e0c6] hover:from-[#008a99] hover:to-[#00c6af] text-black font-medium"
+                onClick={getCertObject}
+              >
+                <ArrowDownToLine className="mr-2 h-4 w-4" />
+              test
               </Button>
             </CardFooter>
           </Card>
