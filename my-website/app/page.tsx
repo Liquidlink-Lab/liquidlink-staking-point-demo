9028d25ea915feb88371032d50c246ca1c459de9
Aaron <ru19wu06@gmail.com>
2025-06-02 12:45:28 +0800

    fix facicon

--- my-website/app/page.tsx
+++ my-website/app/page.tsx
@@ -10,6 +10,7 @@ import { getFullnodeUrl } from '@iota/iota-sdk/client';
 // import { NetworkStatus } from "@/components/network-status"
 import dynamic from 'next/dynamic';
 
+
 const WalletProvider = dynamic(
   () => import('@iota/dapp-kit').then((mod) => mod.WalletProvider),
   {
@@ -41,10 +42,12 @@ function Insite() {
     <div className="min-h-screen bg-black text-gray-200">
       <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-900 to-black opacity-50 z-0"></div>
       <div className="relative z-10">
-        <Wallet />
+    
         <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+          
           <main className="py-8">
-            <h1 className="text-4xl font-bold text-center mb-2 text-white">
+            <h1 className="text-4xl font-bold text-center mb-2 text-white flex items-center justify-center gap-2">
+           
               Liquid <span className="text-[#9763e0]">DeFi</span> Protocol
             </h1>
             <p className="text-center text-gray-400 mb-8">
