e82333292819ee6f21c20b96f38c7e933cc3b414
Chi <76777953+LI-YONG-QI@users.noreply.github.com>
2025-06-11 16:45:05 +0800

    refactor: update staking target and package IDs in configuration

--- my-website/lib/config.ts
+++ my-website/lib/config.ts
@@ -1,9 +1,9 @@
 export const PACKAGE_ID =
-  '0xdd051fe12344d3c1ec76766d9b6cda2274c45f316fd9e79234f37ba2722ca213';
+  '0x2cf341d32644d538f5bd4a654e95bba35df7f9013f29830b73e6a0f6d988f9af';
 export const ACTION = 'stake';
 export const LOCK_PERIOD = 86400 * 1000;
 export const TREASURY_CAP_OBJECT_ID =
-  '0x3bb8bd79671b28aef33ff1b8d113e446a6db0e6fb19441928d4047735be71d06';
+  '0xfaf19c81cd2c67f8920e7836a0750d4d9a9f6e60bdc6463a1b6ad1bb7922c057';
 export const VAULT_OBJECT_ID =
-  '0x50ced78cad583782542faee933382e48516255894d1b1b692dff80397200312e';
+  '0x1b36cfbaeaedfce615e18f49c4ab636ea24a25155d71833d4aa7ad869388ab15';
 export const CERT_TYPE = `0x2::coin::Coin<${PACKAGE_ID}::cert::CERT>`;
