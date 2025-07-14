4ac69e563b4dbbf8982410f6ddc27f8f51a99f39
Chi <76777953+LI-YONG-QI@users.noreply.github.com>
2025-07-14 13:02:27 +0800

    refactor: update package IDs and version in configuration files

--- my-website/lib/config.ts
+++ my-website/lib/config.ts
@@ -1,4 +1,6 @@
 export const PACKAGE_ID =
+  '0x448a02ca9fdfdb5f3895b9bf37c9d03e408df62c452c3e7252f4d496b189aaa0';
+export const ORIGIN_PACKAGE_ID =
   '0x81c6b0dcbb366afdc1604b9f8c82b483f95a58819fb84762ac3a40c6d4340810';
 export const ACTION = 'stake';
 export const LOCK_PERIOD = 86400 * 1000;
@@ -6,4 +8,4 @@ export const TREASURY_CAP_OBJECT_ID =
   '0xdda55b678d3afc06e51baa6f72d270cff2a98763fb733f4448f0b066fcdae19e';
 export const VAULT_OBJECT_ID =
   '0xb551b86a9c058826531315ab70f253aefea81731d3dc24a3eccb19b55ec81056';
-export const CERT_TYPE = `0x2::coin::Coin<${PACKAGE_ID}::cert::CERT>`;
+export const CERT_TYPE = `0x2::coin::Coin<${ORIGIN_PACKAGE_ID}::cert::CERT>`;
